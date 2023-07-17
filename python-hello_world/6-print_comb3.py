69ad0a335b7b8b86f3a6482511fc3c6bfb196373
Gwendolyn <wilkistagwendolyn@gmail.com>
2023-07-18 00:54:50 +0300

    Update

--- python-hello_world/6-print_comb3.py
+++ python-hello_world/6-print_comb3.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
 for tens_digit in range(10):
     for ones_digit in range(tens_digit + 1, 10):
-        print("{:d}{:d}".format(tens_digit, ones_digit), end=", ", flush=True)
+        if tens_digit == 8 and ones_digit == 9:
+            print("{:d}{:d}".format(tens_digit, ones_digit),
+                  end="\n", flush=True)
+        else:
+            print("{:d}{:d}".format(tens_digit, ones_digit),
+                  end=", ", flush=True)
